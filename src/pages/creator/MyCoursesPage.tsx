62ccb04ee8dc8dd3767c263808b6bb4be2623786
Karolis <karolis@localhost.localdomain>
2023-09-22 16:26:00 +0300

    MyCourses page grid layout

--- src/pages/creator/MyCoursesPage.tsx
+++ src/pages/creator/MyCoursesPage.tsx
@@ -8,11 +8,53 @@ function MyCoursesPage() {
             return <section>An error has occured</section>
         }
         return (
-            <section>
+            <section className="grid auto-cols-[400px] auto-rows-[200px] gap-10 grid-rows-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 p-5">
                 {data !== undefined &&
                     data.map((course) => (
                         <Link to={`/myCourses/${course.id}/description`}>{course.name}</Link>
                     ))}
+                <Link
+                    className="flex flex-col p-10 border rounded-2xl text-center align-middle w-[300px]"
+                    to={`/addCourse`}>
+                    <span className="material-symbols-outlined text-[100px]">add_circle</span>
+                    <span>New Course</span>
+                </Link>
+                <Link
+                    className="flex flex-col p-10 border rounded-2xl text-center align-middle w-[300px]"
+                    to={`/addCourse`}>
+                    <span className="material-symbols-outlined text-[100px]">add_circle</span>
+                    <span>New Course</span>
+                </Link>
+                <Link
+                    className="flex flex-col p-10 border rounded-2xl text-center align-middle w-[300px]"
+                    to={`/addCourse`}>
+                    <span className="material-symbols-outlined text-[100px]">add_circle</span>
+                    <span>New Course</span>
+                </Link>
+                <Link
+                    className="flex flex-col p-10 border rounded-2xl text-center align-middle w-[300px]"
+                    to={`/addCourse`}>
+                    <span className="material-symbols-outlined text-[100px]">add_circle</span>
+                    <span>New Course</span>
+                </Link>
+                <Link
+                    className="flex flex-col p-10 border rounded-2xl text-center align-middle w-[300px]"
+                    to={`/addCourse`}>
+                    <span className="material-symbols-outlined text-[100px]">add_circle</span>
+                    <span>New Course</span>
+                </Link>
+                <Link
+                    className="flex flex-col p-10 border rounded-2xl text-center align-middle w-[300px]"
+                    to={`/addCourse`}>
+                    <span className="material-symbols-outlined text-[100px]">add_circle</span>
+                    <span>New Course</span>
+                </Link>
+                <Link
+                    className="flex flex-col p-10 border rounded-2xl text-center align-middle w-[300px]"
+                    to={`/addCourse`}>
+                    <span className="material-symbols-outlined text-[100px]">add_circle</span>
+                    <span>New Course</span>
+                </Link>
             </section>
         )
     }
